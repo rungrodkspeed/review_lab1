89b2e0f7782ab567fc1eae80f6f8233a587c0beb
unknown <rungrodks@hotmail.com>
2019-01-21 15:51:32 +0700

    https://github.com/rungrodkspeed/review_lab1

---
+++ review_lab1_4.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main(){
+    int year;
+    std::cout << "Please input year of birth: ";
+    std::cin >> year;
+    std:: cout << "Your age = "<< 2019-year << " years old";
+    return 0;
+}
